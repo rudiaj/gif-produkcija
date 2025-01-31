21d3ee23b87b9c158252c89eec618b883ce2f555
rudiaj <aj.rudi@gmail.com>
2025-01-31 19:46:39 +0100

    fix: button

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -20,9 +20,9 @@ const Navigation = () => {
       className="sticky top-0 z-20 mb-32"
     >
       <div className="container mx-auto px-4">
-        <div className="flex justify-between items-center py-6">
+        <div className="flex justify-between items-center py-3">
           <Image alt="logo" src="/images/logo.svg" width={32} height={32} />
-          <button className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 px-6 text-white bg-black transition-all duration-100 [box-shadow:5px_5px_rgb(255_255_255)] active:translate-x-[3px] active:translate-y-[3px] active:[box-shadow:0px_0px_rgb(255_255_255)]">
+          <button className="group relative inline-flex h-10 items-center justify-center overflow-hidden rounded-md border border-neutral-200 px-6 text-white bg-black transition-all duration-100 [box-shadow:5px_5px_rgb(255_255_255)] active:translate-x-[3px] active:translate-y-[3px] active:[box-shadow:0px_0px_rgb(255_255_255)]">
             Contact
           </button>
         </div>
